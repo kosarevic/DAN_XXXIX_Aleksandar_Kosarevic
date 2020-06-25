6a7cfe8ae099ce0ef71dedc0545bdf0bfe0ccb13
Kosarevic <kosarevic@gmail.com>
2020-06-25 10:32:54 +0200

    Added AddNewSong() and Write() methods.

--- Zadatak_1/Program.cs
+++ Zadatak_1/Program.cs
@@ -9,8 +9,8 @@ namespace Zadatak_1
 {
     class Program
     {
-        static List<Song> songs = new List<Song>();
-        static int Id = 0;
+        public static List<Song> songs = new List<Song>();
+        public static int Id = 0;
 
         static void Main(string[] args)
         {
@@ -20,7 +20,8 @@ namespace Zadatak_1
             {
                 Console.WriteLine("1. Read all songs.");
                 Console.WriteLine("2. Add new song.");
-                Console.WriteLine("3. Exit");
+                Console.WriteLine("3. Music player.");
+                Console.WriteLine("4. Exit");
                 Console.WriteLine();
                 Console.WriteLine("Chose an option:");
                 option = Console.ReadLine();
@@ -29,40 +30,39 @@ namespace Zadatak_1
                 switch (option)
                 {
                     case "1":
-                        foreach (Song s in songs)
-                        {
-                            Console.WriteLine("{0} {1} {2}",s.Author, s.Title, s.Length);
-                        }
+                        Service s = new Service();
+                        s.ReadAllSongs();
                         Console.WriteLine();
                         break;
                     case "2":
-                        Console.WriteLine("Insert author of the song.");
-                        string 
+                        s = new Service();
+                        s.AddNewSong();
+                        Write();
+                        Console.WriteLine();
+                        break;
+                    case "3":
+                        s = new Service();
                         break;
                     default:
                         Console.WriteLine("Incorect input, please try again.\n");
                         break;
                 }
 
-            } while (option != "3");
+            } while (option != "4");
         }
 
         static void Read()
         {
             List<string> text = new List<string>();
-
             StreamReader sr = new StreamReader("..//../Files/Music.txt");
             string line;
             while ((line = sr.ReadLine()) != null)
             {
                 text.Add(line);
             }
-
             sr.Close();
-
             if (text.Any())
             {
-
                 foreach (string t in text)
                 {
                     string[] temp = t.Split(':','[',']');
@@ -78,7 +78,15 @@ namespace Zadatak_1
 
         static void Write()
         {
+            string[] output = new string[songs.Count];
+            string line = "";
 
+            for (int i = 0; i < songs.Count; i++)
+            {
+                line = "[" + songs[i].Author + "]:[" + songs[i].Title + "][" + songs[i].Length + "]";
+                output[i] = line;
+            }
+            File.WriteAllLines("..//../Files/Music.txt", output);
         }
     }
 }
---
+++ Zadatak_1/Service.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_1
+{
+    class Service
+    {
+
+        public void ReadAllSongs()
+        {
+            foreach (Song s in Program.songs)
+            {
+                Console.WriteLine("{0} {1} {2}", s.Author, s.Title, s.Length);
+            }
+        }
+
+        public void AddNewSong()
+        {
+            string input;
+            Song s = new Song();
+            s.Id = ++Program.Id;
+            Console.WriteLine("Please insert song author.");
+            input = Console.ReadLine();
+            s.Author = input;
+            Console.WriteLine("Please insert song title.");
+            input = Console.ReadLine();
+            s.Title = input;
+            Console.WriteLine("Please insert duration of the song. (hh)");
+            int hours = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please insert duration of the song. (mm)");
+            int minutes = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please insert duration of the song. (ss)");
+            int seconds = int.Parse(Console.ReadLine());
+            s.Length = new TimeSpan(hours, minutes, seconds);
+            Program.songs.Add(s);
+        }
+
+        public void MusicPlayer()
+        {
+
+        }
+
+    }
+}
