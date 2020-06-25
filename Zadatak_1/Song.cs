55522a4900b23a9cea895529d9d0b7b2cb7047a3
Kosarevic <kosarevic@gmail.com>
2020-06-25 09:12:11 +0200

    Adeed Music.txt and created Song Class

---
+++ Zadatak_1/Song.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_1
+{
+    class Song
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public DateTime Length { get; set; }
+
+        public Song(int id, string author, string title, DateTime length)
+        {
+            Id = id;
+            Author = author;
+            Title = title;
+            Length = length;
+        }
+
+        public Song()
+        {
+        }
+    }
+}
